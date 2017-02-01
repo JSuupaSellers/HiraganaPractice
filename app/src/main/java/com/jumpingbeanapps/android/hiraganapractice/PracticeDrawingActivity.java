e5f3d8f1b3e308a7d130c25e2f483c2e690929bb
JSuupaSellers <joshua.sellersdq@gmail.com>
2017-02-01 13:40:16 -0500

    Finished Drawing Canvas

--- app/src/main/java/com/jumpingbeanapps/android/hiraganapractice/PracticeDrawingActivity.java
+++ app/src/main/java/com/jumpingbeanapps/android/hiraganapractice/PracticeDrawingActivity.java
@@ -1,11 +1,137 @@
 package com.jumpingbeanapps.android.hiraganapractice;
 
+import android.content.res.Resources;
+import android.media.Image;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+import java.util.List;
 
 /**
  * Created by Josh on 1/31/2017.
  */
 
 public class PracticeDrawingActivity extends AppCompatActivity {
+    private SoundSetup mSoundSetup;
+    private Sound currentSound;
+    private RecyclerView mLetters;
+    private ImageButton mPlay;
+    private ImageButton mNew;
+    private ImageView traceImage;
+    private DrawingView drawingView;
+    private int resourceId;
+    private AdView mAdView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.drawing_view_layout);
+        currentSound = null;
+        mPlay = (ImageButton)findViewById(R.id.play_btn);
+        mNew = (ImageButton)findViewById(R.id.clear_btn);
+        drawingView = (DrawingView)findViewById(R.id.drawing);
 
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
+        mSoundSetup = new SoundSetup(this);
+        mLetters = (RecyclerView)findViewById(R.id.hiragana_letters_recycler_view);
+        mLetters.setLayoutManager(layoutManager);
+        mLetters.setAdapter(new SoundAdapter(mSoundSetup.getSounds()));
+        traceImage = (ImageView)findViewById(R.id.letter_to_draw);
+
+        MobileAds.initialize(getApplicationContext(), "ca-app-pub-7438194096520605~9660046770");
+
+        mAdView = (AdView)findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+        mPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(currentSound != null) {
+                    mSoundSetup.play(currentSound);
+                }else{
+                    return;
+                }
+            }
+        });
+        mNew.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawingView.startNew();
+            }
+        });
+    }
+
+    private class SoundHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        private TextView mLetterName;
+        private ImageView mLetterImage;
+
+        private Sound mSound;
+
+        public SoundHolder(LayoutInflater inflater, ViewGroup container){
+            super(inflater.inflate(R.layout.list_item_sound,container,false));
+
+            mLetterName = (TextView)itemView.findViewById(R.id.letter);
+            mLetterImage = (ImageView)itemView.findViewById(R.id.letter_image);
+            mLetterImage.setOnClickListener(this);
+        }
+
+        public void bindSound(Sound sound){
+            mSound = sound;
+            mLetterName.setText(mSound.getName());
+            Resources res = getResources();
+            int id = res.getIdentifier(mSound.getName(), "drawable", getPackageName());
+            mLetterImage.setImageResource(id);
+        }
+
+        @Override
+        public void onClick(View v){
+            currentSound = mSound;
+            Resources res = getResources();
+            int id = res.getIdentifier(mSound.getName(),"drawable",getPackageName());
+            traceImage.setImageResource(id);
+            drawingView.startNew();
+        }
+
+
+
+    }
+    private class SoundAdapter extends RecyclerView.Adapter<SoundHolder>{
+        private List<Sound> mSounds;
+
+        public SoundAdapter(List<Sound> sounds){
+            mSounds = sounds;
+        }
+        @Override
+        public SoundHolder onCreateViewHolder(ViewGroup parent, int viewType){
+            LayoutInflater inflater = getLayoutInflater();
+            return new SoundHolder(inflater,parent);
+        }
+        @Override
+        public void onBindViewHolder(SoundHolder soundHolder, int position){
+
+            Sound sound = mSounds.get(position);
+            soundHolder.bindSound(sound);
+
+        }
+        @Override
+        public int getItemCount(){
+            return mSounds.size();
+        }
+    }
 }
---
+++ app/src/main/java/com/jumpingbeanapps/android/hiraganapractice/SoundSetup.java
@@ -0,0 +1,71 @@
+package com.jumpingbeanapps.android.hiraganapractice;
+
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Josh on 1/31/2017.
+ */
+
+public class SoundSetup {
+    private static final String TAG = "SoundSetup";
+    private static final String SOUNDS_FOLDER = "hiragana_sounds";
+    private static final int MAX_SOUNDS = 1;
+
+    private SoundPool mSoundPool;
+    private AssetManager mAssets;
+    private List<Sound> mSounds = new ArrayList<>();
+    public SoundSetup(Context context){
+        mAssets = context.getAssets();
+        mSoundPool = new SoundPool(MAX_SOUNDS, AudioManager.STREAM_MUSIC,0);
+        loadSounds();
+    }
+
+    private void loadSounds(){
+        String[] soundNames;
+        try{
+            soundNames = mAssets.list(SOUNDS_FOLDER);
+            Log.i(TAG, "Found " + soundNames.length + " sounds");
+        }catch(IOException ioe){
+            Log.e(TAG,"Cound not list assets", ioe);
+            return;
+        }
+        for(String filename : soundNames){
+            try{
+                String assetPath = SOUNDS_FOLDER + "/" + filename;
+                Sound sound = new Sound(assetPath);
+                load(sound);
+                mSounds.add(sound);
+            }catch (IOException ioe){
+                Log.e(TAG,"Could not load sound " + filename, ioe);
+            }
+
+        }
+    }
+
+    private void load(Sound sound)throws IOException{
+        AssetFileDescriptor afd = mAssets.openFd(sound.getAssetPath());
+        int soundId = mSoundPool.load(afd,1);
+        sound.setSoundId(soundId);
+    }
+
+    public void play(Sound sound){
+        Integer soundId = sound.getSoundId();
+        if(soundId == null){
+            return;
+        }
+        mSoundPool.play(soundId, 1.0f, 1.0f, 1, 0, 1.0f);
+    }
+
+    public List<Sound> getSounds(){
+        return mSounds;
+    }
+}
